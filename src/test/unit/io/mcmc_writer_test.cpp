fcfc9e5b3248173c8cdcba6e64f67f179c8a7507
Peter Li <Li.Peter94@gmail.com>
2014-07-08 10:55:32 -0400

    Revert "changed sample to std::vector<sample> in mcmc_writer"
    This reverts commit bafa4b48d1515918994dd4d283eda3886d42a99a.

--- src/test/unit/io/mcmc_writer_test.cpp
+++ src/test/unit/io/mcmc_writer_test.cpp
@@ -83,10 +83,8 @@ TEST(StanIoMcmcWriter, write_sample_params) {
   double log_prob = 3.14;
   double accept_stat = 0.84;
   
-  stan::mcmc::sample sample0(real, log_prob, accept_stat);
-  std::vector<stan::mcmc::sample> sample;
-  sample.push_back(sample0);
-
+  stan::mcmc::sample sample(real, log_prob, accept_stat);
+  
   // Sampler
   typedef boost::ecuyer1988 rng_t;
   rng_t base_rng(0);
@@ -297,10 +295,8 @@ TEST(StanIoMcmcWriter, write_diagnostic_params) {
   double log_prob = 3.14;
   double accept_stat = 0.84;
   
-  stan::mcmc::sample sample0(real, log_prob, accept_stat);
-  std::vector<stan::mcmc::sample> sample;
-  sample.push_back(sample0);
-
+  stan::mcmc::sample sample(real, log_prob, accept_stat);
+  
   // Sampler
   typedef boost::ecuyer1988 rng_t;
   rng_t base_rng(0);
