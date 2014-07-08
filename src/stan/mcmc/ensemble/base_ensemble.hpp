da7b93cfecefe58823e5f31e6de202c496b1dbd0
Peter Li <Li.Peter94@gmail.com>
2014-07-08 13:24:02 -0400

    added base_ensemble with tests

---
+++ src/stan/mcmc/ensemble/base_ensemble.hpp
@@ -0,0 +1,182 @@
+#ifndef __STAN__MCMC__ENSEMBLE__BASE_ENSEMBLE_HPP__
+#define __STAN__MCMC__ENSEMBLE__BASE_ENSEMBLE_HPP__
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include <stan/mcmc/base_mcmc.hpp>
+#include <stan/prob/distributions/univariate/continuous/normal.hpp>
+#include <stan/prob/distributions/univariate/continuous/uniform.hpp>
+
+namespace stan {
+  
+  namespace mcmc {
+    
+    // Ensemble Sampler
+        
+    template <class M, class BaseRNG>
+    class base_ensemble: public base_mcmc {
+      
+    public:
+      
+      base_ensemble(M& m, BaseRNG& rng, std::ostream* o, std::ostream* e)
+        : base_mcmc(o,e), 
+          _model(m),
+          _params_mean(m.num_params_r()),
+          _current_states(2*m.num_params_r()+1),
+          _new_states(2*m.num_params_r()+1),
+          _logp(2*m.num_params_r()+1),
+          _accept_prob(2*m.num_params_r()+1),
+          _rand_int(rng),
+          _rand_uniform(_rand_int),
+          _scale(2.0) {};  
+ 
+      ~base_ensemble() {};
+
+      virtual void write_metric(std::ostream* o) = 0;
+      virtual void ensemble_transition(std::vector<Eigen::VectorXd>& cur_states,
+                                       std::vector<Eigen::VectorXd>& new_states,
+                                       Eigen::VectorXd& logp,
+                                       Eigen::VectorXd& accept_prob) = 0;
+   
+      void write_sampler_state(std::ostream* o) {
+        if(!o) return;
+        *o << "# Scale = " << get_scale() << std::endl;
+        this->write_metric(o);
+      }
+      
+      void get_sampler_diagnostic_names(std::vector<std::string>& model_names,
+                                        std::vector<std::string>& names) {
+        this->get_param_names(model_names, names);
+      }
+      
+      void get_sampler_diagnostics(std::vector<double>& values) {
+        this->get_params(values);
+      }
+
+      void get_params(std::vector<double>& values) {
+        for(size_t i = 0; i < _params_mean.size(); ++i)
+          values.push_back(_params_mean(i));
+       }
+      
+      void get_param_names(std::vector<std::string>& model_names,
+                           std::vector<std::string>& names) {
+        for(size_t i = 0; i < _params_mean.size(); ++i)
+          names.push_back(model_names[i]);
+       }
+
+      void write_sampler_param_names(std::ostream& o) {
+        o << "scale__,";
+      }
+      
+      void write_sampler_params(std::ostream& o) {
+        o << this->_scale << ",";
+      }
+      
+      void get_sampler_param_names(std::vector<std::string>& names) {
+        names.push_back("scale__");
+      }
+      
+      void get_sampler_params(std::vector<double>& values) {
+        values.push_back(this->_scale);
+      }
+      
+      void set_scale(const double e) {
+        if(e > 0) _scale = e;
+      }
+      
+      double get_scale() { return this->_scale; }
+      
+      double sample_z() {
+        return pow(stan::prob::uniform_rng(0.0, 1.0, _rand_int) * (_scale -1)
+                   + 1, 2) / _scale;
+      }
+
+      double log_prob(Eigen::VectorXd& q) {
+        try {
+          _model.stan::model::prob_grad_ad::log_prob(q, this->_err_stream);
+        } catch (std::domain_error e) {
+          this->_write_error_msg(this->_err_stream, e);
+          return std::numeric_limits<double>::infinity();
+        }
+        return _model.stan::model::prob_grad_ad::log_prob(q, this->_err_stream);
+      }
+
+      sample transition(sample& init_sample) {
+        _params_mean.setZero();
+        _logp.setZero();
+        _accept_prob.setZero();
+        for (int i = 0; i < _new_states.size(); i++) 
+          _new_states[i].setZero();
+
+        ensemble_transition(_current_states, _new_states, _logp, _accept_prob);
+
+        for (int j = 0; j < _params_mean.size(); j++) {
+          for (int i = 0; i < _current_states.size(); i++)
+            _params_mean(j) += _new_states[i](j) / _current_states.size();
+        }
+
+        _current_states = _new_states;
+
+        return sample(_params_mean, _logp.mean(), _accept_prob.mean());
+      }
+
+      void initialize_ensemble() {
+        for (int i = 0; i < _current_states.size(); i++) {
+          _current_states[i].resize(_params_mean.size());
+          _new_states[i].resize(_params_mean.size());
+          for (int j = 0; j < _current_states[i].size(); j++) {
+            _current_states[i](j) = _rand_uniform() - 0.5;
+          }
+        }
+        
+        _params_mean.setZero();
+        for (int j = 0; j < _params_mean.size(); j++) {
+          for (int i = 0; i < _current_states.size(); i++)
+            _params_mean(j) += _current_states[i](j) / _current_states.size();
+        }     
+      }
+
+
+    protected:
+      M _model;
+
+      Eigen::VectorXd _params_mean;
+      std::vector<Eigen::VectorXd> _current_states;
+      std::vector<Eigen::VectorXd> _new_states;
+
+      Eigen::VectorXd _logp;
+      Eigen::VectorXd _accept_prob;
+
+      BaseRNG& _rand_int;
+      boost::uniform_01<BaseRNG&> _rand_uniform;                
+
+      double _scale;
+
+      void _write_error_msg(std::ostream* error_msgs,
+                           const std::domain_error& e) {
+          if (!error_msgs) return;
+          
+          *error_msgs << std::endl
+                      << "Informational Message: The parameter state is about to be Metropolis"
+                      << " rejected due to the following underlying, non-fatal (really)"
+                      << " issue (and please ignore that what comes next might say 'error'): "
+                      << e.what()
+                      << std::endl
+                      << "If the problem persists across multiple draws, you might have"
+                      << " a problem with an initial state."
+                      << std::endl
+                      << " If the problem does not persist, the resulting samples will still"
+                      << " be drawn from the posterior."
+                      << std::endl;
+      }
+
+    };
+    
+  } // mcmc
+  
+} // stan
+
+
+#endif
---
+++ src/test/unit/mcmc/ensemble/base_ensemble_test.cpp
@@ -0,0 +1,296 @@
+#include <stan/mcmc/ensemble/base_ensemble.hpp>
+#include <boost/random/additive_combine.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/math/distributions.hpp>
+#include <test/test-models/no-main/optimization/rosenbrock.cpp>
+#include <gtest/gtest.h>
+
+typedef boost::ecuyer1988 rng_t;
+typedef rosenbrock_model_namespace::rosenbrock_model Model;
+
+namespace stan {
+  
+  namespace mcmc {
+    
+    class mock_ensemble: public base_ensemble<Model,
+                                              rng_t> {
+      
+    public:
+      
+      mock_ensemble(Model& m, rng_t& rng, std::ostream* o, std::ostream* e) : 
+        base_ensemble<Model,rng_t>
+        (m, rng, o, e)
+      { this->_name = "Mock Ensemble"; }
+      
+      void write_metric(std::ostream* o) {}
+
+      void ensemble_transition(std::vector<Eigen::VectorXd>& cur_states,
+                               std::vector<Eigen::VectorXd>& new_states,
+                               Eigen::VectorXd& logp,
+                               Eigen::VectorXd& accept_prob) {
+        for (int i = 0; i < 5; i++) {
+          logp(i) = 0.5+i;
+          accept_prob(i) = 0.3+i;
+          for (int j = 0; j < 2; j++)
+            new_states[i](j) = cur_states[i](j) + 1;
+        }
+      }
+
+      int get_params_size() {return this->_params_mean.size();}
+      int get_current_states_size() {return this->_current_states.size();}
+      int get_new_states_size() {return this->_new_states.size();}
+      int get_logp_size() {return this->_logp.size();}
+      int get_accept_prob_size() {return this->_accept_prob.size();}
+      double get_scale_init() {return this->_scale;}
+      
+      Eigen::VectorXd get_params_mean() {return this->_params_mean;}
+      Eigen::VectorXd get_accept_prob() {return this->_accept_prob;}
+      Eigen::VectorXd get_logp() {return this->_logp;}
+      std::vector<Eigen::VectorXd> get_current_states() {
+        return this->_current_states;
+      }
+      std::vector<Eigen::VectorXd> get_new_states() {
+        return this->_new_states;
+      }
+    };
+    
+  }
+  
+}  
+
+rng_t base_rng(0);
+ 
+std::stringstream output, error;
+
+static const std::string DATA = "";
+std::stringstream data_stream(DATA);
+stan::io::dump dummy_context(data_stream);
+
+Model model(dummy_context);
+
+stan::mcmc::mock_ensemble sampler(model, base_rng, &output, &error);
+
+TEST(McmcBaseEnsemble, construction) {
+  EXPECT_FLOAT_EQ(2, sampler.get_params_size());
+  EXPECT_FLOAT_EQ(2*2+1, sampler.get_current_states_size());
+  EXPECT_FLOAT_EQ(2*2+1, sampler.get_new_states_size());
+  EXPECT_FLOAT_EQ(2*2+1, sampler.get_logp_size());
+  EXPECT_FLOAT_EQ(2*2+1, sampler.get_accept_prob_size());
+  EXPECT_FLOAT_EQ(2, sampler.get_scale_init());
+  EXPECT_EQ("", output.str());
+  EXPECT_EQ("", error.str());
+}
+
+TEST(McmcBaseEnsemble, write_sampler_state) {
+  sampler.write_sampler_state(&output);
+  EXPECT_TRUE("# Scale = 2\n" == output.str());
+}
+
+TEST(McmcBaseEnsemble, get_sampler_diagnostic_names) {
+  std::vector<std::string> test_model_names;
+  std::vector<std::string> test_names;
+  test_model_names.push_back("x");
+  test_model_names.push_back("y");
+
+  sampler.get_sampler_diagnostic_names(test_model_names, test_names);
+  EXPECT_FLOAT_EQ(2, test_names.size());
+  EXPECT_FLOAT_EQ(2, test_model_names.size());
+
+  EXPECT_TRUE("x" == test_model_names[0]);
+  EXPECT_TRUE("y" == test_model_names[1]);
+  EXPECT_TRUE("x" == test_names[0]);
+  EXPECT_TRUE("y" == test_names[1]);
+}
+
+TEST(McmcBaseEnsemble, get_sampler_diagnostics) {
+  std::vector<double> test_values;
+
+  sampler.get_sampler_diagnostics(test_values);
+  EXPECT_FLOAT_EQ(2, test_values.size());
+}
+
+TEST(McmcBaseEnsemble, get_params) {
+  std::vector<double> test_values;
+
+  sampler.get_params(test_values);
+  EXPECT_FLOAT_EQ(2, test_values.size());
+}
+
+TEST(McmcBaseEnsemble, get_params_names) {
+  std::vector<std::string> test_model_names;
+  std::vector<std::string> test_names;
+  test_model_names.push_back("x");
+  test_model_names.push_back("y");
+
+  sampler.get_param_names(test_model_names, test_names);
+  EXPECT_FLOAT_EQ(2, test_names.size());
+  EXPECT_FLOAT_EQ(2, test_model_names.size());
+
+  EXPECT_TRUE("x" == test_model_names[0]);
+  EXPECT_TRUE("y" == test_model_names[1]);
+  EXPECT_TRUE("x" == test_names[0]);
+  EXPECT_TRUE("y" == test_names[1]);
+}
+
+TEST(McmcBaseEnsemble, write_sampler_param_names) {
+  std::stringstream output2;
+
+  sampler.write_sampler_param_names(output2);
+  EXPECT_TRUE("scale__,"==output2.str());
+}
+
+TEST(McmcBaseEnsemble, write_sampler_params) {
+  std::stringstream output3;
+
+  sampler.write_sampler_params(output3);
+  EXPECT_TRUE("2,"==output3.str());
+}
+
+TEST(McmcBaseEnsemble, get_sampler_param_names) {
+  std::vector<std::string> test_names;
+
+  sampler.get_sampler_param_names(test_names);
+  EXPECT_FLOAT_EQ(1, test_names.size());
+  EXPECT_TRUE("scale__" == test_names[0]);
+}
+
+TEST(McmcBaseEnsemble, get_sampler_params) {
+  std::vector<double> values;
+
+  sampler.get_sampler_params(values);
+  EXPECT_FLOAT_EQ(1, values.size());
+  EXPECT_FLOAT_EQ(2, values[0]);
+}
+
+TEST(McmcBaseEnsemble, get_scale) {
+  EXPECT_FLOAT_EQ(2, sampler.get_scale());
+}
+
+TEST(McmcBaseEnsemble, set_scale) {
+  EXPECT_FLOAT_EQ(2, sampler.get_scale());
+  EXPECT_NO_THROW(sampler.set_scale(3));
+  EXPECT_FLOAT_EQ(3, sampler.get_scale());
+}
+
+TEST(McmcBaseEnsemble, sample_z) {
+  boost::random::mt19937 rng;
+  int N = 10000;
+  int K = boost::math::round(2 * std::pow(N, 0.4));
+  boost::math::chi_squared mydist(K-1);
+
+  double loc[K - 1];
+  for(int i = 1; i < K; i++)
+    loc[i - 1] = std::pow(i * std::pow(K, -1.0)*(sampler.get_scale()-1)+1,2)
+      /sampler.get_scale();
+
+  int count = 0;
+  int bin [K];
+  double expect [K];
+  for(int i = 0 ; i < K; i++) {
+    bin[i] = 0;
+    expect[i] = N / K;
+  }
+
+  while (count < N) {
+    double a = sampler.sample_z();
+    int i = 0;
+    while (i < K-1 && a > loc[i]) 
+      ++i;
+    ++bin[i];
+    count++;
+   }
+
+  double chi = 0;
+
+  for(int j = 0; j < K; j++)
+    chi += ((bin[j] - expect[j]) * (bin[j] - expect[j]) / expect[j]);
+
+  EXPECT_TRUE(chi < quantile(complement(mydist, 1e-6)));
+
+}
+
+TEST(McmcBaseEnsemble, initialize_ensemble) {
+  sampler.initialize_ensemble();
+
+  EXPECT_FLOAT_EQ(2*2+1, sampler.get_current_states_size());
+  EXPECT_FLOAT_EQ(2, sampler.get_params_size());
+
+  std::vector<Eigen::VectorXd> cur_states = sampler.get_current_states();
+  Eigen::VectorXd param_means = sampler.get_params_mean();
+  Eigen::VectorXd calc_param_means(2);
+  calc_param_means.setZero();
+
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 5; i++)
+       calc_param_means(j) += cur_states[i](j) / 5.0;
+  }     
+
+  EXPECT_FLOAT_EQ(calc_param_means(0), param_means(0));
+  EXPECT_FLOAT_EQ(calc_param_means(1), param_means(1));
+}
+
+TEST(McmcBaseEnsemble, initialize_chi_square_goodness_of_fit) {
+  boost::random::mt19937 rng;
+  int N = 10000;
+  int K = boost::math::round(2 * std::pow(N, 0.4));
+  boost::math::chi_squared mydist(K-1);
+  boost::math::uniform_distribution<>dist (-0.5,0.5);
+
+  double loc[K - 1];
+  for(int i = 1; i < K; i++)
+    loc[i - 1] = quantile(dist, i * std::pow(K, -1.0));
+
+  for (int i = 0; i < sampler.get_current_states_size(); i++) {
+    for (int j = 0; j < sampler.get_current_states()[i].size(); j++) {
+      int count = 0;
+      int bin [K];
+      double expect [K];
+      for(int i = 0 ; i < K; i++) {
+        bin[i] = 0;
+        expect[i] = N / K;
+      }
+
+      while (count < N) {
+        sampler.initialize_ensemble();
+        double a = sampler.get_current_states()[i](j);
+        int i = 0;
+        while (i < K-1 && a > loc[i]) 
+          ++i;
+        ++bin[i];
+        count++;
+      }
+      
+      double chi = 0;
+      
+      for(int j = 0; j < K; j++)
+        chi += ((bin[j] - expect[j]) * (bin[j] - expect[j]) / expect[j]);
+      
+      EXPECT_TRUE(chi < quantile(complement(mydist, 1e-6)));
+    }
+  } 
+}
+
+TEST(McmcBaseEnsemble, transition) {
+  Eigen::VectorXd a(2);
+  a<<1,-1;
+  stan::mcmc::sample s0(a,-1,0.3);
+  sampler.initialize_ensemble();
+  std::vector<Eigen::VectorXd> initial_states = sampler.get_current_states();
+  Eigen::VectorXd initial_param_mean = sampler.get_params_mean();
+  stan::mcmc::sample s_avg = sampler.transition(s0);
+
+  for (int i = 0; i < 5; i++) {
+    EXPECT_FLOAT_EQ(0.5+i, sampler.get_logp()(i));
+    EXPECT_FLOAT_EQ(0.3+i, sampler.get_accept_prob()(i));
+
+    for (int j = 0; j < 2; j++) {
+      EXPECT_FLOAT_EQ(sampler.get_current_states()[i](j),
+                      sampler.get_new_states()[i](j));
+      EXPECT_FLOAT_EQ(initial_states[i](j)+1,
+                      sampler.get_new_states()[i](j));
+      EXPECT_FLOAT_EQ(initial_param_mean(j)+1, sampler.get_params_mean()(j));
+    }
+  }
+
+}
