6833e4b293c9d18018928de49793ed45f56e7216
Peter Li <Li.Peter94@gmail.com>
2014-07-09 14:13:14 -0400

    added walk move_ensemble with tests

---
+++ src/stan/mcmc/ensemble/walk_move_ensemble.hpp
@@ -0,0 +1,115 @@
+#ifndef __STAN__MCMC__ENSEMBLE__WALK_MOVE_ENSEMBLE_HPP__
+#define __STAN__MCMC__ENSEMBLE__WALK_MOVE_ENSEMBLE_HPP__
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include <stan/mcmc/ensemble/base_ensemble.hpp>
+#include <stan/prob/distributions/univariate/discrete/bernoulli.hpp>
+
+namespace stan {
+  
+  namespace mcmc {
+    
+    // Ensemble Sampler
+        
+    template <class M, class BaseRNG>
+    class walk_move_ensemble: public base_ensemble<M,BaseRNG> {
+      
+    public:
+      
+      walk_move_ensemble(M& m, BaseRNG& rng,
+                            std::ostream* o = &std::cout, 
+                            std::ostream* e = 0)
+        : base_ensemble<M,BaseRNG>(m,rng,o,e) {
+        this->_name = "Ensemble Sampler using Walk Move";
+        this->initialize_ensemble();
+      } 
+
+      // returns vector from 1 to num_walkers
+      std::vector<int> choose_walkers(int& index, int& num_walkers) {
+        std::vector<int> walkers;
+
+        while (walkers.size() <= 1) {
+          walkers.resize(0);
+          for (int i = 0; i < num_walkers - 1; i++) {
+            int temp = stan::prob::bernoulli_rng(0.5,this->_rand_int);
+            if (temp) {
+              if (i >= index)
+                walkers.push_back(i+2);
+              else
+                walkers.push_back(i+1);
+            }
+          }
+        }
+
+        return walkers;
+      }
+
+      // walker_index is a vector of values ranging from 1 to num_walkers
+      Eigen::VectorXd mean_walkers(std::vector<int>& walker_index, 
+                                   std::vector<Eigen::VectorXd>& cur_states) {
+
+        Eigen::VectorXd mean(cur_states[0].size());
+        mean.setZero();
+
+        for (int i = 0; i < mean.size(); i++) {
+          for (int j = 0; j < walker_index.size(); j++)
+            mean[i] += cur_states[walker_index[j]-1](i) / walker_index.size();
+        }
+        
+        return mean;
+      }
+      
+      void ensemble_transition(std::vector<Eigen::VectorXd>& cur_states,
+                               std::vector<Eigen::VectorXd>& new_states,
+                               Eigen::VectorXd& logp,
+                               Eigen::VectorXd& accept_prob) {
+        for (int i = 0; i < cur_states.size(); i++) {
+
+          //calculate initial logprob for walker i
+          double logp0 = this->log_prob(cur_states[i]);
+
+          //generates index of random walker that is not currently being moved
+          int num_walkers = cur_states.size();
+          std::vector<int> rand_walkers = choose_walkers(i, num_walkers);
+
+          Eigen::VectorXd mean_rand_walkers = mean_walkers(rand_walkers, cur_states);
+
+          //proposes new walker position
+          for (int j = 0; j < rand_walkers.size(); j++) 
+            new_states[i] += this->sample_z() * (cur_states[rand_walkers[j]]
+                                                 - mean_rand_walkers);
+
+          //calculate new log prob
+          logp(i) = this->log_prob(new_states[i]);
+
+          if (boost::math::isnan(logp(i))) 
+            logp(i) = std::numeric_limits<double>::infinity();
+
+          //calculate MH accept_prob based on Goodman and Weare paper
+          accept_prob(i) = std::exp(logp(i) - logp0);
+
+          accept_prob(i) = accept_prob(i) > 1 ? 1 : accept_prob(i);
+
+          if (this->_rand_uniform() > accept_prob(i)) {
+            new_states[i] = cur_states[i];
+            logp(i) = logp0;
+          }
+        }
+     }
+
+      void write_metric(std::ostream* o) {
+        if(!o) return;
+        *o << "# No free parameters for stretch move ensemble sampler" << std::endl;
+      }
+
+    };
+    
+  } // mcmc
+  
+} // stan
+
+
+#endif
---
+++ src/test/unit/mcmc/ensemble/walk_move_ensemble_test.cpp
@@ -0,0 +1,203 @@
+#include <stan/mcmc/ensemble/walk_move_ensemble.hpp>
+#include <boost/random/additive_combine.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/math/distributions.hpp>
+#include <test/test-models/no-main/optimization/rosenbrock.cpp>
+#include <gtest/gtest.h>
+
+typedef boost::ecuyer1988 rng_t;
+typedef rosenbrock_model_namespace::rosenbrock_model Model;
+
+namespace stan {
+  
+  namespace mcmc {
+    
+    class mock_ensemble: public walk_move_ensemble<Model,
+                                                   rng_t> {
+      
+    public:
+      
+      mock_ensemble(Model& m, rng_t& rng, std::ostream* o, std::ostream* e) : 
+        walk_move_ensemble<Model,rng_t>
+        (m, rng, o, e)
+      { this->_name = "Mock Ensemble with Walk Move"; }
+      
+      int get_params_size() {return this->_params_mean.size();}
+      int get_current_states_size() {return this->_current_states.size();}
+      int get_new_states_size() {return this->_new_states.size();}
+      int get_logp_size() {return this->_logp.size();}
+      int get_accept_prob_size() {return this->_accept_prob.size();}
+      double get_scale_init() {return this->_scale;}
+      
+      Eigen::VectorXd get_params_mean() {return this->_params_mean;}
+      Eigen::VectorXd get_accept_prob() {return this->_accept_prob;}
+      Eigen::VectorXd get_logp() {return this->_logp;}
+      std::vector<Eigen::VectorXd> get_current_states() {
+        return this->_current_states;
+      }
+      std::vector<Eigen::VectorXd> get_new_states() {
+        return this->_new_states;
+      }
+    };
+    
+  }
+  
+}  
+
+rng_t base_rng(0);
+ 
+std::stringstream output, error;
+
+static const std::string DATA = "";
+std::stringstream data_stream(DATA);
+stan::io::dump dummy_context(data_stream);
+
+Model model(dummy_context);
+
+stan::mcmc::mock_ensemble sampler(model, base_rng, &output, &error);
+
+TEST(McmcEnsembleWalkMoveEnsemble, construction) {
+  EXPECT_FLOAT_EQ(2*2+1, sampler.get_current_states_size());
+  EXPECT_FLOAT_EQ(2, sampler.get_params_size());
+
+  std::vector<Eigen::VectorXd> cur_states = sampler.get_current_states();
+  Eigen::VectorXd param_means = sampler.get_params_mean();
+  Eigen::VectorXd calc_param_means(2);
+  calc_param_means.setZero();
+
+  for (int j = 0; j < sampler.get_params_mean().size(); j++) {
+    for (int i = 0; i < sampler.get_current_states().size(); i++)
+      calc_param_means(j) += cur_states[i](j) / sampler.get_current_states().size();
+  }     
+
+  EXPECT_FLOAT_EQ(calc_param_means(0), param_means(0));
+  EXPECT_FLOAT_EQ(calc_param_means(1), param_means(1));
+}
+
+TEST(McmcEnsembleWalkMoveEnsemble, construction_chi_square_goodness_of_fit) {
+  boost::random::mt19937 rng;
+  int N = 10000;
+  int K = boost::math::round(2 * std::pow(N, 0.4));
+  boost::math::chi_squared mydist(K-1);
+  boost::math::uniform_distribution<>dist (-0.5,0.5);
+
+  double loc[K - 1];
+  for(int i = 1; i < K; i++)
+    loc[i - 1] = quantile(dist, i * std::pow(K, -1.0));
+
+  for (int i_ = 0; i_ < sampler.get_current_states_size(); i_++) {
+    for (int j_ = 0; j_ < sampler.get_current_states()[i_].size(); j_++) {
+      int count = 0;
+      int bin [K];
+      double expect [K];
+      for(int i = 0 ; i < K; i++) {
+        bin[i] = 0;
+        expect[i] = N / K;
+      }
+
+      while (count < N) {
+        sampler.initialize_ensemble();
+        double a = sampler.get_current_states()[i_](j_);
+        int i = 0;
+        while (i < K-1 && a > loc[i]) 
+          ++i;
+        ++bin[i];
+        count++;
+      }
+      
+      double chi = 0;
+      
+      for(int j = 0; j < K; j++)
+        chi += ((bin[j] - expect[j]) * (bin[j] - expect[j]) / expect[j]);
+      
+      EXPECT_TRUE(chi < quantile(complement(mydist, 1e-6)));
+    }
+  } 
+}
+
+TEST(McmcEnsembleWalkMoveEnsemble, transition) {
+  Eigen::VectorXd a(2);
+  a<<1,-1;
+  stan::mcmc::sample s0(a,-1,0.3);
+  std::vector<Eigen::VectorXd> init_states = sampler.get_current_states();
+
+  stan::mcmc::sample s_new = sampler.transition(s0);
+  std::vector<Eigen::VectorXd> new_states = sampler.get_current_states();
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 2; j++) 
+      std::cout<<"Before: "<<init_states[i](j)<<"  New:  " << new_states[i](j)<<std::endl;
+  }
+}
+
+TEST(McmcEnsembleWalkMoveEnsemble, write_metric) {
+  sampler.write_metric(&output);
+  EXPECT_TRUE("# No free parameters for stretch move ensemble sampler\n"
+              == output.str());
+}
+
+TEST(McmcEnsembleWalkMoveEnsemble, choose_walker) {
+  boost::random::mt19937 rng;
+  int N = 10000;
+  for (int n_walker = 3; n_walker < 20; n_walker++) {
+    for (int i_walker = 0; i_walker < n_walker; i_walker++) {
+      int K = n_walker;
+      boost::math::chi_squared mydist(K-1);
+      int loc[K - 1];
+      for(int i = 1; i < K; i++)
+        loc[i - 1] = i;
+
+      int count = 0;
+      int bin [K];
+      double expect [K];
+      for(int i = 0 ; i < K; i++) {
+        bin[i] = 0;
+        expect[i] = N / (std::pow(2, n_walker-1) - n_walker) 
+          * (std::pow(2, n_walker-2) - 1);
+      }
+    
+      expect[i_walker] = 1;
+
+      while (count < N) {
+        std::vector<int> temp = sampler.choose_walkers(i_walker, n_walker);
+        for (int k_ = 0; k_ < temp.size(); k_++) {
+          int a = temp[k_];
+          int i = 0;
+          while (i < K-1 && a > loc[i])
+            ++i;
+          ++bin[i];
+        }
+        count++;
+      }
+      
+      double chi = 0;
+      
+      for(int j = 0; j < K; j++)
+        chi += ((bin[j] - expect[j]) * (bin[j] - expect[j]) / expect[j]);
+
+      EXPECT_TRUE(chi < quantile(complement(mydist, 1e-6)));
+    }
+  }
+}
+
+TEST(McmcEnsembleWalkMoveEnsemble, mean_walker) {
+  sampler.initialize_ensemble();
+  std::vector<Eigen::VectorXd> temp = sampler.get_current_states();
+  std::vector<int> walker_index;
+  walker_index.push_back(1);
+  walker_index.push_back(3);
+  walker_index.push_back(4);
+  walker_index.push_back(5);
+
+  Eigen::VectorXd means = sampler.mean_walkers(walker_index, temp);
+  Eigen::VectorXd calc_means(means.size());
+  calc_means.setZero();
+
+  for (int i = 0; i < walker_index.size(); i++) {
+    for (int j = 0; j < means.size(); j++) 
+      calc_means(j) += temp[walker_index[i]-1](j) / walker_index.size();
+  }
+
+  for (int i = 0; i < means.size(); i++)
+    EXPECT_FLOAT_EQ(calc_means(i), means(i));
+}
